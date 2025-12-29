72bfc515d57548c3f160240b7576858688c9591d
Nischaya <nischayabeniwal@gmail.com>
2025-12-29 19:08:34 +0530

    Added if_else.cpp & oddeven.cpp

---
+++ if_else + loops/ifelse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = -1;
+    if(n>=0){
+        cout << "n is +ve\n";
+    }else{
+        cout << "n is -ve\n";
+    }
+    return 0;
+}
