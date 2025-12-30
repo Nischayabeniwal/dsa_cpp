7647917b0aff3b9a7eb537c5d6bdd66954bec019
Nischaya <nischayabeniwal@gmail.com>
2025-12-30 17:41:51 +0530

    Added patterns dir

---
+++ patterns/pseudocode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 4; 
+    for(int i =0;i<n;i++){
+        int m = 4;
+        for(int j=0;j<m;j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+
