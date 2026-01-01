0156f1adc4f1fb6b67783878e4bbb91ed441c040
Nischaya <nischayabeniwal@gmail.com>
2026-01-01 19:07:34 +0530

    Added till rev tri

---
+++ patterns/chartri.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 7; 
+    char num = 'A';
+    for(int i =0;i<n;i++){
+        
+        for(int j=0;j<i+1;j++){
+            cout << num << " ";
+            num=num+1;
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+
